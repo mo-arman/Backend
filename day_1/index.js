c33ef36988817fa02781ed33ae2d6a1567b720d8
mo-arman <moharman3818@gmail.com>
2025-02-12 12:48:45 +0530

    add

---
+++ day_1/index.js
@@ -0,0 +1,14 @@
+console.log("Welcome to ws")
+console.log(10+30)
+
+let a = 908;
+let b = 890;
+
+console.log(a+b);
+
+let arr = [1,2,3,4,5,6,7];
+arr.forEach((Val,index)=>{
+    console.log(Val,"=>",index);
+
+})
+
